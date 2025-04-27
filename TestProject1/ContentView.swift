5b3012e5df7af51b0bf4a8f85890f77ea7692f8b
yzhang <zhangyehua1996@gmail.com>
2025-04-26 19:29:12 -0500

    Initial Commit

---
+++ TestProject1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestProject1
+//
+//  Created by Yehua Zhang on 4/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestProject1/TestProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  TestProject1App.swift
+//  TestProject1
+//
+//  Created by Yehua Zhang on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TestProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
